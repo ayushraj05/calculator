887e7738b4a8eb071694737c3bd24f69b400b6c4
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-12-13 16:33:26 +0530

    add logic

--- calculator/Controller/ViewController.swift
+++ calculator/Controller/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,43 +15,57 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLable: UILabel!
     
-    var isFirstNumber: Bool = true
-    
-    @IBAction func numberPressed(_ sender: UIButton) {
-        if let numVal = sender.titleLabel?.text {
-            if isFirstNumber {
-                displayLable.text = numVal
-                isFirstNumber = false
+    private var isFinishedTypingNumber: Bool = true
+        
+        private var displayValue: Double {
+            get {
+                guard let number = Double(displayLable.text!) else {
+                    fatalError("Cannot convert display label text to a Double.")
+                }
+                return number
             }
-            else{
-                if numVal == "." {
-                    if displayLable.text?.contains(".") == false {
-                        displayLable.text?.append(numVal)
-                    }
-                }
-                else{
-                    displayLable.text?.append(numVal)
-                }
+            set {
+                displayLable.text = String(newValue)
             }
         }
+        
+        private var calculator = CalculatorLogic()
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+               
+               if let numValue = sender.currentTitle {
+                   
+                   if isFinishedTypingNumber {
+                       displayLable.text = numValue
+                       isFinishedTypingNumber = false
+                   } else {
+                       
+                       if numValue == "." {
+                           
+                           let isInt = floor(displayValue) == displayValue
+                           
+                           if !isInt {
+                               return
+                           }
+                       }
+                       displayLable.text = displayLable.text! + numValue
+                   }
+               }
     }
     
     @IBAction func calcPressed(_ sender: UIButton) {
-        isFirstNumber = true
-        guard let number = Double(displayLable.text!) else {
-            fatalError("No number in display")
-        }
-        if let calcMethod = sender.titleLabel?.text {
-            if calcMethod == "+/-"  {
-                displayLable.text = String(number * -1)
-            }
-            else if calcMethod == "%" {
-                displayLable.text = String(number / 100)
-            }
-            else if calcMethod == "AC" {
-                displayLable.text = "0"
-            }
-        }
+                
+                isFinishedTypingNumber = true
+                
+                calculator.setNumber(displayValue)
+                
+                if let calcMethod = sender.currentTitle {
+         
+                    if let result = calculator.calculate(symbol: calcMethod) {
+                        displayValue = result
+                    }
+                }
     }
     
     
