82c8ee11bee06e5b84bd878add5c5911308d4e25
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-12-13 14:29:08 +0530

    bug fix

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var displayLable: UILabel!
+    
+    var isFirstNumber: Bool = true
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let numVal = sender.titleLabel?.text {
+            if isFirstNumber {
+                displayLable.text = numVal
+                isFirstNumber = false
+            }
+            else{
+                displayLable.text?.append(numVal)
+            }
+        }
+    }
+    
+    @IBAction func calcPressed(_ sender: UIButton) {
+        isFirstNumber = true
+    }
+    
+    
 }
 
